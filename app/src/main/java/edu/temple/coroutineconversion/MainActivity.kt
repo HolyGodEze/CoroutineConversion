28c23ecda509be611684faadefc5532ee3be3494
Tam Trang <tuo98490@temple.edu>
2024-03-27 15:37:52 -0400

    Refactored the code so it doesn't use a handler and thread

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,6 +6,12 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         cakeImageView.alpha = it.what / 100f
         true
     })
-
+    val scope = CoroutineScope(Job() + Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,12 +31,21 @@ class MainActivity : AppCompatActivity() {
         cakeImageView = findViewById(R.id.imageView)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
+            scope.launch{
                 repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+                    withContext(Dispatchers.Main){
+                        cakeImageView.alpha = it / 100f
+                    }
+                    //handler.sendEmptyMessage(it)
+                    delay(40)
                 }
-            }.start()
+            }
+//            Thread{
+//                repeat(100) {
+//                    handler.sendEmptyMessage(it)
+//                    Thread.sleep(40)
+//                }
+//            }.start()
         }
     }
 }
